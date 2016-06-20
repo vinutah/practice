487dca957f4cd3b04e324219f93971c0c8514258
vinutah <vinu@cs.utah.edu>
2016-06-20 15:06:07 -0600

    om namah sivai

---
+++ cpp_study/main.cpp
@@ -0,0 +1,17 @@
+/* filename : output.cpp
+ * author   : vinu joseph
+ * date     : 20 june 2016
+ * modified :
+ * purpose  : this c++ program prints some message on the screen
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout << "start of c++ prog" << endl ;
+    cout << endl << "i can print whatever i like" << endl;
+    cout << endl << "end of purpose" << endl ;
+
+    return 0;
+}
