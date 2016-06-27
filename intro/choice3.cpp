d596ca3a839193e6e2dd73f1b8eaa7ae823964b5
vinutah <vinu@cs.utah.edu>
2016-06-27 17:01:33 -0600

    added new sources if-else-if else decision trees

---
+++ intro/choice3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "enter a" << endl ;
+    cin >> a;
+
+    if (a > 0){
+        cout << a << "is positive" << endl;
+    } else if (a < 0){
+        cout << a << "is negative" <<  endl ;
+    } else
+        cout << a << "a is zero" <<  endl ;
+
+    return 0;
+}
