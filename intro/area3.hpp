6746e423c1233284f49c2752a1ff23fb49d51778
vinutah <vinutah.soc@gmail.com>
2016-06-29 13:14:24 -0600

    adding hpp file

---
+++ intro/area3.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// function prototypes
+
+void explanation(void);
+void getValues(float & length, float & width);
+float computeArea(float length, float width);
+void printArea(float area);
